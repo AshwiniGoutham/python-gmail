d6ad81dc7629a379de63e69f22fcb021f89da6c4
AshwiniGoutham <37098406+AshwiniGoutham@users.noreply.github.com>
2019-02-08 12:19:47 +0530

    Update email.py

--- email.py
+++ email.py
@@ -1,23 +1,43 @@
 import smtplib
+from file.constants import *
+from email.mime.text import MIMEText
 
 def sendMail():
     try:
-        fromuser = "xyz@gmail.com"
-        to = "xyz@gmail.com"
-        username = "xyz@gmail.com"
-        password = "**********"
+        name = username
+        passw = password
+
         server = smtplib.SMTP('smtp.gmail.com:587')
         server.ehlo()
         server.starttls()
-        server.login(username, password)
+        server.login(name, passw)
         msg = "Hello! Welcome"
-        server.sendmail(fromuser, to, msg)
+        server.sendmail(name, name, msg)
         server.close()
         print("mail sent successfuly")
     except Exception as e:
         print("Failed to send mail:",e)
 
+
+def sendmailsubject():
+    msg = MIMEText("Hello!! Send mail with Subject")
+
+    # me == the sender's email address
+    # you == the recipient's email address
+    msg['Subject'] = 'Here goes subject'
+    msg['From'] = username
+    msg['To'] = username
+
+    # Send the message via our own SMTP server, but don't include the
+    # envelope header.
+    s = smtplib.SMTP('smtp.gmail.com:587')
+    print("Sending mail")
+    s.ehlo()
+    s.starttls()
+    s.login(username, password)
+    s.sendmail(username, username, msg.as_string())
+    print("Mail sent")
+    s.quit()
+
+sendmailsubject()
 sendMail()
-
-
-
